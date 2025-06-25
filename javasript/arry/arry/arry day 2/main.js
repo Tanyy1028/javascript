e89539ee8ce7abcf10c70e466cbe4114e0a625c0
Tanyy1028 <tanmaycpatil1028@gmail.com>
2025-06-25 17:40:05 +0530

    Add files via upload

---
+++ javasript/arry/arry/arry day 2/main.js
@@ -0,0 +1,131 @@
+// ARRAY :- array is a one type of variable. it's used to store multiple value.
+
+//print the array.
+// let n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// for (let i = 0; i < n.length; i++) {
+//     document.writeln(n[i]);
+// }
+
+
+
+//print only even numbers of array - filter
+
+// let n = [99, 200, 299, 400, 399, 600, 699, 800, 899, 1000];
+
+// for (let i = 0; i < n.length; i++) {
+//     if (n[i] % 2 == 0) {
+//         document.writeln(n[i]);
+//     }
+// }
+
+
+
+// show products which price is more than 100.
+
+// let price = [300, 566, 99, 699, 299, 30];
+// for (let i = 0; i < price.length; i++) {
+//     if (price[i] > 100) {
+//         document.writeln(price[i]);
+//     }
+// }
+
+
+
+
+// print sum of array.
+// let n = [36, 46, 668, 542, 4];
+// let sum = 0;
+// for (let i = 0; i < n.length; i++) {
+//     sum = sum + n[i];
+// }
+// document.writeln(sum);
+
+
+
+
+// printf reverse array.
+
+// let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = n.length - 1; i >= 0; i--) {
+//     document.writeln(n[i]);
+// }
+
+
+//Perform addition between two array.
+
+// let a = [33, 67, 43, 76, 95];
+// let b = [65, 20, 49, 10, 64];
+// for (let i = 0; i < a.length; i++) {
+//     document.writeln(a[i] + b[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javasript/arry/arry/arry day1/main.js
@@ -0,0 +1,80 @@
+
+// wap to find the maximum element of array using method
+// let num = [10, 20, 30, 400, 50, 60];
+// let newnum = num.sort((a, b) => b - a);
+// for (let i = 0; i < 1; i++) {
+//     document.writeln(newnum[i])
+// }
+
+
+// wap to find maximum element of array without using method.
+// let max = [10, 20, 30, 40, 500, 60];
+// let maxnum = max[0]
+// for (i = 1; i < max.length; i++) {     
+//     if (max[i] > maxnum) {
+//         maxnum = max[i]
+//     }
+// }
+// document.writeln(maxnum)
+
+
+
+// wap to find minimum element of array using method.
+// let num = [ 10, 20, 30, 40, 50, 60 , 5  ];
+// let minnum = num[0];
+// for(let i=1; i<num.length;i++){
+//     if(num[i]< minnum){
+//         minnum = num[i]
+//     }
+// }
+// document.writeln(minnum);
+
+
+// wap to sort array in decending order without using method.        
+
+// let nums = [15, 58, 23, 1, 10, 42, 59, 63, 99, 100, 29, 74]
+
+// for (var i = 0; i <= nums.length; i++) {
+//     for (var j = i + 1; j <= nums.length; j++) {
+//         if (nums[i] < nums[j]) {
+//             var temp = nums[i]
+//             nums[i] = nums[j]
+//             nums[j] = temp
+//         }
+//     }
+// }
+
+// document.writeln(nums)
+
+
+
+//// wap to sort array in acending order using method.
+
+// var arr = [20, 54, 2, 34, 19, 70, 500, 443, 32, 221, 100, 165]
+// arr.sort((a, b) => a - b)
+// document.writeln(arr)
+
+
+// wap to sort array in decending order using method.
+
+// var arr = [20, 54, 2, 34, 19, 70, 500, 443, 32, 221, 100, 165]
+// arr.sort((a, b) => b - a)
+// document.writeln(arr)
+
+
+// wap to find the maximum element of array using method.
+
+// var arr = [3, 70, 2, 9, 5];
+// var max = arr.sort((a, b) => b - a)[0];
+// document.writeln(max);
+
+
+
+// wap to find the minimum element of array using method.
+
+// var arr = [3, 70, 2, 9, 5];
+// var min = arr.sort((a, b) => a - b)[0];
+// document.writeln(min);
+
+
+
